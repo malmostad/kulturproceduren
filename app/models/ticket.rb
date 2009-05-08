d5a9511fad055f7fc10f840e0967cdb405422822
niklas <niklas@24dc916a-b473-6eb2-fcd1-c0615b0c82f1>
2009-05-08 07:57:32 +0000

    constants
    git-svn-id: http://svn.dev.malmo.se/svn-e-tjanster/web-apps/kp/trunk@298 24dc916a-b473-6eb2-fcd1-c0615b0c82f1

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,14 @@
 class Ticket < ActiveRecord::Base
+  #State Declarations, "constants"
+
+  CREATED          = 0
+  ALLOTED_GROUPS   = 1
+  ALLOTED_DISTRICT = 2
+  FREE_FOR_ALL     = 3
+  BOOKED           = 4
+  USED             = 5
+  NOT_USED         = 6
+
   belongs_to :Occasion
   belongs_to :Event
   belongs_to :District
