28f7f2e6ca074d2128b8a59fac3c3b35e2a2d7a6
andreas <andreas@24dc916a-b473-6eb2-fcd1-c0615b0c82f1>
2009-05-13 10:03:29 +0000

    Skapade unittester för user-modellen.
    git-svn-id: http://svn.dev.malmo.se/svn-e-tjanster/web-apps/kp/trunk@316 24dc916a-b473-6eb2-fcd1-c0615b0c82f1

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,10 +1,44 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
 
-  test "the truth" do
-    assert true
+  test "successful authentication" do
+    u = User.authenticate("foo", "foopass")
+
+    assert_not_nil u
+    assert_equal u.username, "foo"
+  end
+
+  test "invalid password" do
+    u = User.authenticate("foo", "fail")
+    assert_nil u
+  end
+
+  test "invalid username" do
+    u = User.authenticate("notexist", "notexist")
+    assert_nil u
+  end
+
+
+  test "password creation" do
+    pass = "passcreatepass"
+    u = User.new
+    u.username = "passcreateuser"
+    u.password = pass
+    u.save
+
+    u2 = User.authenticate(u.username, pass)
+
+    assert_not_nil u2
+    assert_equal u2.username, u.username
+  end
+
+  test "unique username" do
+    u = User.new
+    u.username = "foo"
+    u.password = "foo"
+
+    assert !u.save
   end
   
 end
