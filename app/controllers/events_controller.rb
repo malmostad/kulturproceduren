7d99ab4117778818c832b0088d2ce9b551137263
andreas <andreas@24dc916a-b473-6eb2-fcd1-c0615b0c82f1>
2009-05-25 13:54:56 +0000

    Fixade editfunktionalitet för events.
    git-svn-id: http://svn.dev.malmo.se/svn-e-tjanster/web-apps/kp/trunk@386 24dc916a-b473-6eb2-fcd1-c0615b0c82f1

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,38 +5,24 @@ class EventsController < ApplicationController
   # GET /events.xml
   def index
     @events = Event.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @events }
-    end
   end
 
   # GET /events/1
   # GET /events/1.xml
   def show
     @event = Event.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @event }
-    end
   end
 
   # GET /events/new
   # GET /events/new.xml
   def new
     @event = Event.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @event }
-    end
   end
 
   # GET /events/1/edit
   def edit
     @event = Event.find(params[:id])
+    render :action => "new"
   end
 
   # POST /events
@@ -44,15 +30,11 @@ class EventsController < ApplicationController
   def create
     @event = Event.new(params[:event])
 
-    respond_to do |format|
-      if @event.save
-        flash[:notice] = 'Evenemanget skapades.'
-        format.html { redirect_to(@event) }
-        format.xml  { render :xml => @event, :status => :created, :location => @event }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @event.errors, :status => :unprocessable_entity }
-      end
+    if @event.save
+      flash[:notice] = 'Evenemanget skapades.'
+      redirect_to(@event)
+    else
+      render :action => "new"
     end
   end
 
@@ -61,15 +43,11 @@ class EventsController < ApplicationController
   def update
     @event = Event.find(params[:id])
 
-    respond_to do |format|
-      if @event.update_attributes(params[:event])
-        flash[:notice] = 'Evenemanget uppdaterades.'
-        format.html { redirect_to(@event) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @event.errors, :status => :unprocessable_entity }
-      end
+    if @event.update_attributes(params[:event])
+      flash[:notice] = 'Evenemanget uppdaterades.'
+      redirect_to(@event)
+    else
+      render :action => "new"
     end
   end
 
@@ -79,9 +57,6 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
     @event.destroy
 
-    respond_to do |format|
-      format.html { redirect_to(events_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(events_url)
   end
 end
