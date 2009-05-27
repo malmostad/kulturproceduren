e47c28a9a1c90aeb87f42add6ede8adeafa00215
niklas <niklas@24dc916a-b473-6eb2-fcd1-c0615b0c82f1>
2009-05-27 12:01:10 +0000

    Formulär för att bygga enkäter
    git-svn-id: http://svn.dev.malmo.se/svn-e-tjanster/web-apps/kp/trunk@419 24dc916a-b473-6eb2-fcd1-c0615b0c82f1

--- app/controllers/questionaires_controller.rb
+++ app/controllers/questionaires_controller.rb
@@ -1,6 +1,7 @@
 class QuestionairesController < ApplicationController
   layout "standard"
-  
+  before_filter :authenticate
+  require "pp"
   # GET /questionaires
   # GET /questionaires.xml
   def index
@@ -15,44 +16,57 @@ class QuestionairesController < ApplicationController
   # GET /questionaires/1
   # GET /questionaires/1.xml
   def show
+    @user = User.find_by_id(session[:current_user_id])
     @questionaire = Questionaire.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @questionaire }
+    @all_q = Question.find(:all)
+    if @user.roles.include?(Role.find_by_name("Administratör"))
+      render :admin_view
     end
   end
 
   # GET /questionaires/new
   # GET /questionaires/new.xml
   def new
+    if ! Event.all.collect { |e| e.questionaire.nil? }.include?(true)
+      flash[:error] = "Alla evenamng har redan enkäter!"
+      redirect_to :controller => "questionaires" , :action => "index"
+      return
+    end
     @questionaire = Questionaire.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @questionaire }
+    @user = User.find_by_id(session[:current_user_id])
+    @all_q = Question.find(:all)
+    if @user.roles.include?(Role.find_by_name("Administratör"))
+      render :new
+    else
+      flash[:error] = "RED ALERT - UNAUTHORIZED ACCESS - ALL HANDS TO BATTLE STATIONS"
+      redirect_to "/"
     end
   end
 
   # GET /questionaires/1/edit
   def edit
     @questionaire = Questionaire.find(params[:id])
+    @user = User.find_by_id(session[:current_user_id])
+    @all_q = Question.find(:all)
+    if @user.roles.include?(Role.find_by_name("Administratör"))
+      render :admin_view
+    else
+      flash[:error] = "RED ALERT - UNAUTHORIZED ACCESS - ALL HANDS TO BATTLE STATIONS"
+      redirect_to "/"
+    end
   end
 
   # POST /questionaires
   # POST /questionaires.xml
   def create
     @questionaire = Questionaire.new(params[:questionaire])
-
-    respond_to do |format|
-      if @questionaire.save
-        flash[:notice] = 'Questionaire was successfully created.'
-        format.html { redirect_to(@questionaire) }
-        format.xml  { render :xml => @questionaire, :status => :created, :location => @questionaire }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @questionaire.errors, :status => :unprocessable_entity }
-      end
+    @questionaire.question_ids = Question.find(:all, :conditions => "template is true").collect { |q| q.id }
+    
+    if @questionaire.save
+      flash[:notice] = 'Questionaire was successfully created.'
+      redirect_to @questionaire 
+    else
+      render :action => "new" 
     end
   end
 
@@ -60,16 +74,11 @@ class QuestionairesController < ApplicationController
   # PUT /questionaires/1.xml
   def update
     @questionaire = Questionaire.find(params[:id])
-
-    respond_to do |format|
-      if @questionaire.update_attributes(params[:questionaire])
-        flash[:notice] = 'Questionaire was successfully updated.'
-        format.html { redirect_to(@questionaire) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @questionaire.errors, :status => :unprocessable_entity }
-      end
+    if @questionaire.update_attributes(params[:questionaire])
+      flash[:notice] = 'Questionaire was successfully updated.'
+      redirect_to(@questionaire) 
+    else
+      render :action => "edit" 
     end
   end
 
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,6 @@
 class QuestionsController < ApplicationController
   layout "standard"
+  require "pp"
   
   # GET /questions
   # GET /questions.xml
@@ -37,22 +38,33 @@ class QuestionsController < ApplicationController
   # GET /questions/1/edit
   def edit
     @question = Question.find(params[:id])
+    @questionaire = Questionaire.find_by_id(params[:questionaire_id])
+    @user = User.find_by_id(session[:current_user_id])
+    @all_q = Question.find(:all)
+    if @user.roles.include?(Role.find_by_name("Administratör"))
+      render :template => "questionaires/admin_view"
+    end
+    
   end
 
   # POST /questions
   # POST /questions.xml
   def create
     @question = Question.new(params[:question])
-
-    respond_to do |format|
-      if @question.save
-        flash[:notice] = 'Question was successfully created.'
-        format.html { redirect_to(@question) }
-        format.xml  { render :xml => @question, :status => :created, :location => @question }
+    res = @question.save
+    @questionaire = Questionaire.find_by_id(params[:questionaire_id])
+    a = @questionaire.question_ids
+    a.push(@question.id)
+    @questionaire.question_ids = a
+    if not @questionaire.save
+      flash[:error] = "Kunde inte uppdatera enkäten med nya frågor"
+    end
+    if res
+        flash[:notice] = 'Ny fråga skapad'
+        redirect_to(@questionaire)
       else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @question.errors, :status => :unprocessable_entity }
-      end
+        flash[:error] += " Kunde inte skapa fråga!"
+        redirect_to :controller => "questionaire" , :action => "index"
     end
   end
 
@@ -60,28 +72,24 @@ class QuestionsController < ApplicationController
   # PUT /questions/1.xml
   def update
     @question = Question.find(params[:id])
-
-    respond_to do |format|
-      if @question.update_attributes(params[:question])
-        flash[:notice] = 'Question was successfully updated.'
-        format.html { redirect_to(@question) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @question.errors, :status => :unprocessable_entity }
-      end
+    if @question.update_attributes(params[:question])
+      flash[:notice] = 'Question was successfully updated.'
+      redirect_to(Questionaire.find_by_id(params[:questionaire_id]))
+    else
+      flash[:error] = "Frågan kunde inte uppdateras"
+      redirect_to(Questionaire.find_by_id(params[:questionaire_id]))
     end
   end
 
   # DELETE /questions/1
   # DELETE /questions/1.xml
   def destroy
-    @question = Question.find(params[:id])
+    @question = Question.find(params[:question_id])
     @question.destroy
+    @questionaire = Questionaire.find_by_id(params[:questionaire_id])
 
-    respond_to do |format|
-      format.html { redirect_to(questions_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to @questionaire
+    
+    
   end
 end
