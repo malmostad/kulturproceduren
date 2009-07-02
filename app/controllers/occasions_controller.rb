cf068b76f028bbb3b79a79b0ef5e642a8b1fecd0
niklas <niklas@24dc916a-b473-6eb2-fcd1-c0615b0c82f1>
2009-07-02 08:16:35 +0000

    återrapportering av närvaro - ytterligare felkontroller
    git-svn-id: http://svn.dev.malmo.se/svn-e-tjanster/web-apps/kp/trunk@650 24dc916a-b473-6eb2-fcd1-c0615b0c82f1

--- app/controllers/occasions_controller.rb
+++ app/controllers/occasions_controller.rb
@@ -74,193 +74,201 @@ class OccasionsController < ApplicationController
         :state => Ticket::BOOKED
       } ).map { |t| t.group_id }
     if not params[:groups].blank?
-      @report_complete = params[:groups].keys.map {|k| k.to_i }.sort == @groups.map {|g| g.id}.sort
-      if @report_complete
-        @groups.each do |group|
-          tickets = Ticket.find(:all , :conditions => {:occasion_id => @occasion.id , :group_id => group.id } )
-          n = 0
-          tickets.each do |ticket|
-            if n < params[:groups]["#{group.id}"].to_i
-              ticket.state = Ticket::USED
-            else
-              ticket.state = Ticket::NOT_USED
-            end
-            ticket.save or flash[:error] = "Kunde inte uppdatera närvarostatistiken ..."
-            n += 1
+      @report_complete = false
+      #allow only numerical gids and number of attendees
+      params[:groups].select { |gid,nattend|
+        nattend.is_a? Integer and gid.is_a? Integer
+      }.map { |gids,nattends|
+        @reported_groups["#{gids}"] = nattends
+      }
+      @report_complete = @reported_groups.keys.map {|k| k.to_i }.sort == @groups.map {|g| g.id}.sort
+    end
+    if @report_complete
+      @groups.each do |group|
+        tickets = Ticket.find(:all , :conditions => {:occasion_id => @occasion.id , :group_id => group.id } )
+        n = 0
+        tickets.each do |ticket|
+          if n < params[:groups]["#{group.id}"].to_i
+            ticket.state = Ticket::USED
+          else
+            ticket.state = Ticket::NOT_USED
           end
+          ticket.save or flash[:error] = "Kunde inte uppdatera närvarostatistiken ..."
+          n += 1
         end
-        flash[:notice] = "Tack för närvarorapporten"
-        redirect_to "/"
-      else
-        flash[:error] = "Rapporten inkomplett"
-        render :report
       end
-    end
-  end
-
-
-  def attendants
-    @occasion = Occasion.find(params[:id])
-
-    group_ids = Ticket.find(
-      :all ,
-      :select => "distinct group_id" ,
-      :conditions => {
-        :occasion_id => @occasion.id ,
-        :state => Ticket::BOOKED
-      } ).map { |t| t.group_id }
-
-    if params[:format] == "pdf"
-      @groups = Group.find group_ids
+      flash[:notice] = "Tack för närvarorapporten"
+      redirect_to "/"
     else
-      @groups = Group.paginate group_ids, :page => params[:page], :order => 'updated_at DESC'
+      flash[:error] = "Rapporten inkomplett"
+      render :report
     end
+  end
+end
 
-    @booking_reqs = BookingRequirement.find_all_by_group_id(
-      @groups.map { |g| g.id } ,
-      :conditions => { :occasion_id => @occasion.id }
-    )
 
-    if params[:format] == "pdf"
-      pdf = get_pdf()
-      send_data pdf.render, :filename => "attendants.pdf",:type => "application/pdf" , :disposition => 'inline'
-    end
+def attendants
+  @occasion = Occasion.find(params[:id])
 
+  group_ids = Ticket.find(
+    :all ,
+    :select => "distinct group_id" ,
+    :conditions => {
+      :occasion_id => @occasion.id ,
+      :state => Ticket::BOOKED
+    } ).map { |t| t.group_id }
+
+  if params[:format] == "pdf"
+    @groups = Group.find group_ids
+  else
+    @groups = Group.paginate group_ids, :page => params[:page], :order => 'updated_at DESC'
   end
 
-  def show
-    @selected_occasion = Occasion.find(params[:id])
-    @event = @selected_occasion.event
+  @booking_reqs = BookingRequirement.find_all_by_group_id(
+    @groups.map { |g| g.id } ,
+    :conditions => { :occasion_id => @occasion.id }
+  )
 
-    render :template => "events/show"
+  if params[:format] == "pdf"
+    pdf = get_pdf()
+    send_data pdf.render, :filename => "attendants.pdf",:type => "application/pdf" , :disposition => 'inline'
   end
 
-  def edit
-    @occasion = Occasion.find(params[:id])
+end
 
-    unless current_user.can_administrate?(@occasion.event.culture_provider)
-      flash[:error] = "Du har inte behörighet att komma åt sidan."
-      redirect_to @occasion.event
-      return
-    end
+def show
+  @selected_occasion = Occasion.find(params[:id])
+  @event = @selected_occasion.event
 
+  render :template => "events/show"
+end
+
+def edit
+  @occasion = Occasion.find(params[:id])
+
+  unless current_user.can_administrate?(@occasion.event.culture_provider)
+    flash[:error] = "Du har inte behörighet att komma åt sidan."
+    redirect_to @occasion.event
+    return
+  end
+
+  @event = @occasion.event
+  @category_groups = CategoryGroup.all :order => "name ASC"
+  render :template => "events/show"
+end
+
+def create
+  @occasion = Occasion.new(params[:occasion])
+
+  unless current_user.can_administrate?(@occasion.event.culture_provider)
+    flash[:error] = "Du har inte behörighet att komma åt sidan."
+    redirect_to @occasion.event
+    return
+  end
+
+  if @occasion.save
+    flash[:notice] = 'Föreställningen skapades.'
+    redirect_to(@occasion.event)
+  else
+    flash.now[:error] = 'Fel uppstod när föreställningen skulle skapas.'
     @event = @occasion.event
     @category_groups = CategoryGroup.all :order => "name ASC"
     render :template => "events/show"
   end
+end
 
-  def create
-    @occasion = Occasion.new(params[:occasion])
+def update
+  @occasion = Occasion.find(params[:id])
 
-    unless current_user.can_administrate?(@occasion.event.culture_provider)
-      flash[:error] = "Du har inte behörighet att komma åt sidan."
-      redirect_to @occasion.event
-      return
-    end
-
-    if @occasion.save
-      flash[:notice] = 'Föreställningen skapades.'
-      redirect_to(@occasion.event)
-    else
-      flash.now[:error] = 'Fel uppstod när föreställningen skulle skapas.'
-      @event = @occasion.event
-      @category_groups = CategoryGroup.all :order => "name ASC"
-      render :template => "events/show"
-    end
+  unless current_user.can_administrate?(@occasion.event.culture_provider)
+    flash[:error] = "Du har inte behörighet att komma åt sidan."
+    redirect_to @occasion.event
+    return
   end
 
-  def update
-    @occasion = Occasion.find(params[:id])
-
-    unless current_user.can_administrate?(@occasion.event.culture_provider)
-      flash[:error] = "Du har inte behörighet att komma åt sidan."
-      redirect_to @occasion.event
-      return
-    end
-
-    if @occasion.update_attributes(params[:occasion])
-      flash[:notice] = 'Föreställningen uppdaterades.'
-      redirect_to(@occasion.event)
-    else
-      flash.now[:error] = 'Fel uppstod när föreställningen skulle uppdateras.'
-      @event = @occasion.event
-      @category_groups = CategoryGroup.all :order => "name ASC"
-      render :template => "events/show"
-    end
-  end
-
-  def destroy
-    @occasion = Occasion.find(params[:id])
-
-    if current_user.can_administrate?(@occasion.event.culture_provider)
-      @occasion.destroy
-    else
-      flash[:error] = "Du har inte behörighet att komma åt sidan."
-    end
-
-    flash[:notice] = 'Föreställningen togs bort.'
+  if @occasion.update_attributes(params[:occasion])
+    flash[:notice] = 'Föreställningen uppdaterades.'
     redirect_to(@occasion.event)
+  else
+    flash.now[:error] = 'Fel uppstod när föreställningen skulle uppdateras.'
+    @event = @occasion.event
+    @category_groups = CategoryGroup.all :order => "name ASC"
+    render :template => "events/show"
+  end
+end
+
+def destroy
+  @occasion = Occasion.find(params[:id])
+
+  if current_user.can_administrate?(@occasion.event.culture_provider)
+    @occasion.destroy
+  else
+    flash[:error] = "Du har inte behörighet att komma åt sidan."
   end
 
+  flash[:notice] = 'Föreställningen togs bort.'
+  redirect_to(@occasion.event)
+end
 
-  private
 
-  def get_pdf
+private
 
-    pdf = PDF::Writer.new  :paper => "A4" , :orientation => :landscape
-    pdf.select_font("Helvetica")
-    pdf.margins_cm(2,2,2,2)
+def get_pdf
 
-    PDF::SimpleTable.new do |tab|
-      tab.title = "Deltagarlista för #{@occasion.event.name} #{@occasion.date.to_s}".to_iso
-      tab.column_order.push(*%w(group comp comptel att wheel req pres))
+  pdf = PDF::Writer.new  :paper => "A4" , :orientation => :landscape
+  pdf.select_font("Helvetica")
+  pdf.margins_cm(2,2,2,2)
 
-      tab.columns["group"] = PDF::SimpleTable::Column.new("group") { |col|
-        col.heading = "Skola / Grupp".to_iso
-      }
-      tab.columns["comp"] = PDF::SimpleTable::Column.new("com") { |col|
-        col.heading = "Medföljande vuxen".to_iso
-      }
-      tab.columns["comptel"] = PDF::SimpleTable::Column.new("comptel") { |col|
-        col.heading = "Telefonnummer"
-      }
-      tab.columns["att"] = PDF::SimpleTable::Column.new("att") { |col|
-        col.heading = "Deltagare"
-      }
-      tab.columns["wheel"] = PDF::SimpleTable::Column.new("wheel") { |col|
-        col.heading = "Rullstolsplatser"
-      }
-      tab.columns["req"]  = PDF::SimpleTable::Column.new("req") { |col|
-        col.heading = "Övriga önskemål".to_iso
-      }
-      tab.columns["pres"]  = PDF::SimpleTable::Column.new("pres") { |col|
-        col.heading = "Antal närvarande".to_iso
-      }
+  PDF::SimpleTable.new do |tab|
+    tab.title = "Deltagarlista för #{@occasion.event.name} #{@occasion.date.to_s}".to_iso
+    tab.column_order.push(*%w(group comp comptel att wheel req pres))
 
-      tab.show_lines    = :all
-      tab.show_headings = true
-      #tab.orientation   = :right
-      tab.orientation   = 1
-      tab.position      = :left
-      tab.font_size     = 9
-      tab.maximum_width = 1
-      puts "DEBUGG: #{tab.maximum_width}"
-      data = []
-      @groups.each do |g|
-        row = {}
-        row["group"]   = (g.school.name.to_s + " - " + g.name.to_s).to_iso
-        row["comp"]    = g.companion_by_occasion(@occasion).name.to_iso
-        row["comptel"] = g.companion_by_occasion(@occasion).tel_nr.to_s.to_iso
-        row["att"]     = g.ntickets_by_occasion(@occasion,Ticket::BOOKED).to_s.to_iso
-        row["wheel"]   = g.ntickets_by_occasion(@occasion,Ticket::BOOKED,true).to_s.to_iso
-        row["req"]     = @booking_reqs.select { |b| b.group_id == g.id }.map { |b| (b.requirement.to_s + "\n").to_iso  }
-        row["pres"]    = " ".to_iso
-        data << row
-      end
-      tab.data.replace data
-      tab.render_on(pdf)
+    tab.columns["group"] = PDF::SimpleTable::Column.new("group") { |col|
+      col.heading = "Skola / Grupp".to_iso
+    }
+    tab.columns["comp"] = PDF::SimpleTable::Column.new("com") { |col|
+      col.heading = "Medföljande vuxen".to_iso
+    }
+    tab.columns["comptel"] = PDF::SimpleTable::Column.new("comptel") { |col|
+      col.heading = "Telefonnummer"
+    }
+    tab.columns["att"] = PDF::SimpleTable::Column.new("att") { |col|
+      col.heading = "Deltagare"
+    }
+    tab.columns["wheel"] = PDF::SimpleTable::Column.new("wheel") { |col|
+      col.heading = "Rullstolsplatser"
+    }
+    tab.columns["req"]  = PDF::SimpleTable::Column.new("req") { |col|
+      col.heading = "Övriga önskemål".to_iso
+    }
+    tab.columns["pres"]  = PDF::SimpleTable::Column.new("pres") { |col|
+      col.heading = "Antal närvarande".to_iso
+    }
+
+    tab.show_lines    = :all
+    tab.show_headings = true
+    #tab.orientation   = :right
+    tab.orientation   = 1
+    tab.position      = :left
+    tab.font_size     = 9
+    tab.maximum_width = 1
+    puts "DEBUGG: #{tab.maximum_width}"
+    data = []
+    @groups.each do |g|
+      row = {}
+      row["group"]   = (g.school.name.to_s + " - " + g.name.to_s).to_iso
+      row["comp"]    = g.companion_by_occasion(@occasion).name.to_iso
+      row["comptel"] = g.companion_by_occasion(@occasion).tel_nr.to_s.to_iso
+      row["att"]     = g.ntickets_by_occasion(@occasion,Ticket::BOOKED).to_s.to_iso
+      row["wheel"]   = g.ntickets_by_occasion(@occasion,Ticket::BOOKED,true).to_s.to_iso
+      row["req"]     = @booking_reqs.select { |b| b.group_id == g.id }.map { |b| (b.requirement.to_s + "\n").to_iso  }
+      row["pres"]    = " ".to_iso
+      data << row
     end
-
-    return pdf
+    tab.data.replace data
+    tab.render_on(pdf)
   end
+
+  return pdf
+end
 end
