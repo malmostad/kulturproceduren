243e9120c7d30be69af05a3bf87b81623156a024
andreas <andreas@24dc916a-b473-6eb2-fcd1-c0615b0c82f1>
2009-08-05 13:29:02 +0000

    Layouttweak.
    git-svn-id: http://svn.dev.malmo.se/svn-e-tjanster/web-apps/kp/trunk@680 24dc916a-b473-6eb2-fcd1-c0615b0c82f1

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -23,27 +23,31 @@ class CalendarController < ApplicationController
   end
 
   def apply_filter
-    calendar_filter[:free_text] = params[:filter][:free_text]
-    
-    calendar_filter[:from_age] = (params[:filter][:from_age] || -1).to_i
-    calendar_filter[:to_age] = (params[:filter][:to_age] || -1).to_i
-    calendar_filter[:further_education] = params[:filter][:further_education].to_i == 1
-
-    calendar_filter[:from_date] = parse_date(params[:filter][:from_date])
-    calendar_filter[:to_date] = parse_date(params[:filter][:to_date])
-
-    calendar_filter[:date_span] = case params[:filter][:date_span]
-    when "day" then :day
-    when "week" then :week
-    when "month" then :month
-    when "date" then :date
+    if params[:clear_filter]
+      session[:calendar_filter] = { :from_date => Date.today }
     else
-      :unbounded
+      calendar_filter[:free_text] = params[:filter][:free_text]
+
+      calendar_filter[:from_age] = (params[:filter][:from_age] || -1).to_i
+      calendar_filter[:to_age] = (params[:filter][:to_age] || -1).to_i
+      calendar_filter[:further_education] = params[:filter][:further_education].to_i == 1
+
+      calendar_filter[:from_date] = parse_date(params[:filter][:from_date])
+      calendar_filter[:to_date] = parse_date(params[:filter][:to_date])
+
+      calendar_filter[:date_span] = case params[:filter][:date_span]
+                                    when "day" then :day
+                                    when "week" then :week
+                                    when "month" then :month
+                                    when "date" then :date
+                                    else
+                                      :unbounded
+                                    end
+
+      categories = params[:filter][:categories] || []
+      calendar_filter[:categories] = categories.map{ |i| i.to_i }.select { |i| i != -1 }
+
     end
-
-    categories = params[:filter][:categories] || []
-    calendar_filter[:categories] = categories.map{ |i| i.to_i }.select { |i| i != -1 }
-
     redirect_to :action => "filter", :list => @calendar_list
   end
 
