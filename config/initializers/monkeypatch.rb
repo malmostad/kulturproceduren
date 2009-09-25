09ddbff64e675403b04340bb78652d754eb26c2a
andreas <andreas@24dc916a-b473-6eb2-fcd1-c0615b0c82f1>
2009-09-25 11:53:33 +0000

    Detaljdokumenterade monkeypatchningen av Rails.
    git-svn-id: http://svn.dev.malmo.se/svn-e-tjanster/web-apps/kp/trunk@1024 24dc916a-b473-6eb2-fcd1-c0615b0c82f1

--- config/initializers/monkeypatch.rb
+++ config/initializers/monkeypatch.rb
@@ -8,6 +8,7 @@ module ActionView
     module AssetTagHelper
       private
 
+      # actionpack-2.3.4/lib/action_view/helpers/asset_tag_helper.rb:528
       # Patch to ignore relative_url_root when using an asset host
       def compute_public_path(source, dir, ext = nil, include_host = true)
         has_request = @controller.respond_to?(:request)
@@ -22,6 +23,7 @@ module ActionView
 
           source = rewrite_asset_path(source)
 
+          # MONKEYPATCH: Added check on asset_host
           if has_request && include_host && !ActionController::Base.asset_host
             unless source =~ %r{^#{ActionController::Base.relative_url_root}/}
               source = "#{ActionController::Base.relative_url_root}#{source}"
@@ -45,48 +47,61 @@ module ActionView
 
     class InstanceTag
       private
+      # actionpack-2.3.4/lib/action_view/helpers/form_helper.rb:896
       # Overrides the method generating the DOM ID for a field
       def tag_id
+        # MONKEYPATCH: Added kp- prefix
         "kp-#{sanitized_object_name}-#{sanitized_method_name}"
       end
+      # actionpack-2.3.4/lib/action_view/helpers/form_helper.rb:900
       # Overrides the method generating the DOM ID for a field with an index
       def tag_id_with_index(index)
+        # MONKEYPATCH: Added kp- prefix
         "kp-#{sanitized_object_name}-#{index}-#{sanitized_method_name}"
       end
     end
 
     class DateTimeSelector
       private
+      # actionpack-2.3.4/lib/action_view/helpers/date_helper.rb:875
       # Overrides the DOM ID generator for date/time selectors
       def input_id_from_type(type)
+        # MONKEYPATCH: Added kp- prefix
         "kp-" + input_name_from_type(type).gsub(/([\[\(])|(\]\[)/, '-').gsub(/[\]\)]/, '')
       end
     end
 
     class FormBuilder
+      # actionpack-2.3.4/lib/action_view/helpers/form_helper.rb:900
       # Overrides the method generating the submit button, prefixing the submit button's DOM ID.
       def submit(value = "Save changes", options = {})
+        # MONKEYPATCH: Added kp- prefix to id
         @template.submit_tag(value, options.reverse_merge(:id => "kp-#{object_name}_submit"))
       end
     end
 
     module FormTagHelper
+      # actionpack-2.3.4/lib/action_view/helpers/form_tag_helper.rb:314
       # Overrides the DOM ID generation, prefixing the ID
       def radio_button_tag(name, value, checked = false, options = {})
         pretty_tag_value = value.to_s.gsub(/\s/, "_").gsub(/(?!-)\W/, "").downcase
         pretty_name = name.to_s.gsub(/\[/, "_").gsub(/\]/, "")
+        # MONKEYPATCH: Added kp- prefix to id
         html_options = { "type" => "radio", "name" => name, "id" => "kp-#{pretty_name}_#{pretty_tag_value}", "value" => value }.update(options.stringify_keys)
         html_options["checked"] = "checked" if checked
         tag :input, html_options
       end
 
+      # actionpack-2.3.4/lib/action_view/helpers/form_tag_helper.rb:481
       # Overrides the DOM ID generation to prefix the ID
       def sanitize_to_id(name)
+        # MONKEYPATCH: Added kp- prefix
         "kp-" + name.to_s.gsub(']','').gsub(/[^-a-zA-Z0-9:.]/, "_")
       end
     end
 
     module ActiveRecordHelper
+      # actionpack-2.3.4/lib/action_view/helpers/active_record_helper.rb:109
       # Overrides the error message for a specific field to adhere to
       # the markup guidelines for malmo.se
       def error_message_on(object, method, *args)
@@ -97,8 +112,10 @@ module ActionView
 
           options[:prepend_text] = args[0] || ''
           options[:append_text] = args[1] || ''
+          # MONKEYPATCH: Changed the default CSS class
           options[:css_class] = args[2] || 'validation-error-message alert-field'
         end
+        # MONKEYPATCH: Changed the default CSS class
         options.reverse_merge!(:prepend_text => '', :append_text => '', :css_class => 'validation-error-message alert-field')
 
         if (obj = (object.respond_to?(:errors) ? object : instance_variable_get("@#{object}"))) &&
@@ -118,9 +135,11 @@ end
 
 module ActionController
   module RecordIdentifier
+    # actionpack-2.3.4/lib/action_controller/record_identifier.rb:61
     # Overrides the DOM class generator to prefix the class
     def dom_class(record_or_class, prefix = nil)
       singular = singular_class_name(record_or_class)
+      # MONKEYPATCH: Added kp- prefix
       prefix ? "kp-#{prefix}#{JOIN}#{singular}" : "kp-#{singular}"
     end
   end
