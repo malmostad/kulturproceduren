1cb0bba84e87c6ea6910e2b12d60e6f253022ad1
niklas <niklas@24dc916a-b473-6eb2-fcd1-c0615b0c82f1>
2009-10-27 09:33:12 +0000

    Vettigare seed-script för db
    git-svn-id: http://svn.dev.malmo.se/svn-e-tjanster/web-apps/kp/trunk@1371 24dc916a-b473-6eb2-fcd1-c0615b0c82f1

---
+++ script/seed_db.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+function usage {
+    echo "$0 -e RAILS_ENV -d /path/to/rails/app -r /path/to/rake/command/rake"
+    echo "defaults, -d . -e development -r $(which rake)"
+}
+
+
+argparse="option"
+
+for arg in $*
+do
+    if [ $argparse = "option" ]; then
+	if [ "${arg:0:1}" = '-' ]; then
+	    argparse=$arg
+	else 
+	    echo "no options"
+	    usage
+	    exit -1
+	fi
+    else
+	case $argparse in
+	    "-d")
+		railsdir=$arg
+		if [ ! -d $railsdir ]; then
+		    echo "$railsdir is not a directory"
+		    usage
+		    exit -1
+		fi
+		echo "RAILS_DIR = $railsdir"
+		argparse="option"
+		;;
+	    "-e")
+		railsenv=$arg
+		echo "RAILS_ENV = $railsenv"
+		argparse="option"
+		;;
+	    "-r")
+		rake=$arg
+		if [ ! -x $rake ]; then
+		    echo "$rake is not a executable"
+		    usage
+		    exit -1
+		fi
+		argparse="option"
+		;;
+	    *)
+		usage
+		exit -1
+		;;
+	esac
+    fi
+done
+
+if [ -z $railsdir ]; then
+    railsdir=$(pwd)
+fi
+if [ ! -d ${railsdir}/lib/tasks ]; then
+    echo "$railsdir is not a railsapp directory"
+    usage
+    exit -1
+fi
+if [ -z $railsenv ]; then
+    railsenv="development"
+fi
+if [ -z $rake ]; then
+    rake=$(which rake)
+    if [ ! -x $rake ]; then
+	echo "No valid rake command found"
+	usage
+	exit -1
+    fi
+fi
+
+cd $railsdir
+$rake RAILS_ENV=${railsenv} db:drop
+if [ $? -ne "0" ]; then
+    echo "Unable to drop database - STOP"
+    exit -1
+fi
+$rake RAILS_ENV=${railsenv} db:create
+if [ $? -ne "0" ]; then
+    echo "Unable to create database - STOP"
+    exit -1
+fi
+$rake RAILS_ENV=${railsenv} db:migrate
+if [ $? -ne "0" ]; then
+    echo "Unable to migrate database - STOP"
+    exit -1
+fi
+$rake RAILS_ENV=${railsenv} kp:bootstrap
+if [ $? -ne "0" ]; then
+    echo "Unable to bootstrap application"
+    exit -1
+fi
+
+$rake RAILS_ENV=${railsenv} kp:demo:create_categories
+if [ $? -ne "0" ]; then
+    echo "Unable to bootstrap application"
+    exit -1
+fi
+
+$rake RAILS_ENV=${railsenv} kp:demo:create_group_structure
+if [ $? -ne "0" ]; then
+    echo "Unable to bootstrap application"
+    exit -1
+fi
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12
+do
+    $rake RAILS_ENV=${railsenv} kp:demo:generate_culture_provider
+done
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12
+do
+    $rake RAILS_ENV=${railsenv} kp:demo:generate_event culture_provider_id=$i
+    $rake RAILS_ENV=${railsenv} kp:demo:generate_event culture_provider_id=$i
+    $rake RAILS_ENV=${railsenv} kp:demo:generate_event culture_provider_id=$i
+    $rake RAILS_ENV=${railsenv} kp:demo:generate_standing_event culture_provider_id=$i
+done
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+    $rake RAILS_ENV=${railsenv} kp:demo:create_tickets event_id=$i
+done
+
+$rake RAILS_ENV=${railsenv} kp:demo:create_questions
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+    $rake RAILS_ENV=${railsenv} kp:demo:create_questionnaires event_id=$i
+done
+
+exit 0
