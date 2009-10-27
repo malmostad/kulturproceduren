90c04f7fd8d11b729b7c76e0c293af32056cda20
niklas <niklas@24dc916a-b473-6eb2-fcd1-c0615b0c82f1>
2009-10-27 10:41:07 +0000

    DB-vyer för statistik
    git-svn-id: http://svn.dev.malmo.se/svn-e-tjanster/web-apps/kp/trunk@1373 24dc916a-b473-6eb2-fcd1-c0615b0c82f1

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091026073710) do
+ActiveRecord::Schema.define(:version => 20091027103239) do
 
   create_table "age_groups", :force => true do |t|
     t.integer  "age"
