bb3d557bb82c31a54f9bf3364eab5438bd4e76b6
andreas <andreas@24dc916a-b473-6eb2-fcd1-c0615b0c82f1>
2009-12-16 08:36:07 +0000

    Fixade rättigheter i länkning arrangör-arrangör.
    git-svn-id: http://svn.dev.malmo.se/svn-e-tjanster/web-apps/kp/trunk@1508 24dc916a-b473-6eb2-fcd1-c0615b0c82f1

--- app/controllers/culture_provider_links_controller.rb
+++ app/controllers/culture_provider_links_controller.rb
@@ -4,45 +4,50 @@ class CultureProviderLinksController < ApplicationController
   layout "standard"
 
   before_filter :authenticate
-  before_filter :require_admin
+  before_filter :load_culture_provider
 
   def index
-    @culture_provider = CultureProvider.find params[:culture_provider_id]
   end
 
   def new
-    @culture_provider = CultureProvider.find params[:culture_provider_id]
     @culture_providers = CultureProvider.not_linked_to(@culture_provider).paginate :page => params[:page],
       :order => sort_order("name")
   end
 
   def select
-    from = CultureProvider.find params[:culture_provider_id]
     to = CultureProvider.find params[:id]
 
-    from.linked_culture_providers << to
-    to.linked_culture_providers << from
+    @culture_provider.linked_culture_providers << to
+    to.linked_culture_providers << @culture_provider
 
     flash[:notice] = "Länken mellan arrangörerna skapades."
 
-    redirect_to culture_provider_culture_provider_links_url(:culture_provider_id => from.id)
+    redirect_to culture_provider_culture_provider_links_url(:culture_provider_id => @culture_provider.id)
   end
 
   def destroy
-    from = CultureProvider.find params[:culture_provider_id]
     to = CultureProvider.find params[:id]
 
-    from.linked_culture_providers.delete(to)
-    to.linked_culture_providers.delete(from)
+    @culture_provider.linked_culture_providers.delete(to)
+    to.linked_culture_providers.delete(@culture_provider)
 
     flash[:notice] = "Länken mellan arrangörerna togs bort."
 
-    redirect_to culture_provider_culture_provider_links_url(:culture_provider_id => from.id)
+    redirect_to culture_provider_culture_provider_links_url(:culture_provider_id => @culture_provider.id)
   end
 
   protected
 
+  def load_culture_provider
+    @culture_provider = CultureProvider.find params[:culture_provider_id]
+
+    unless current_user.can_administrate?(@culture_provider)
+      flash[:error] = "Du har inte behörighet att komma åt sidan."
+      redirect_to @culture_provider
+    end
+  end
+
   def sort_column_from_param(p)
-  "name"
+    "name"
   end
 end
