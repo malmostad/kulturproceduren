f1314d1ef9ab410f23b3c6f0aa8cb05937dd5392
Andreas Johansson <andreas@stdin.se>
2013-09-27 07:47:28 +0200

    Hantera ActiveRecord::RecordNotFound

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    render :file => "#{Rails.root}/public/404.html", :layout => false, :status => :not_found
+  end
+
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 
