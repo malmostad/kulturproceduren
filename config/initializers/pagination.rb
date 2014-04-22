5432c36b4b570b295a168b16053e17bbaaa1ff17
Andreas Johansson <andreas@stdin.se>
2014-04-22 09:39:48 +0200

    Uppdaterade will_paginate-modifikationerna.
    Från version 3 så ändrades beteendet i will_paginate. I18n används
    istället för konfigurering, och man måste överlagra will_paginate() så
    att den använder en anpassad linkrenderer.

--- config/initializers/pagination.rb
+++
@@ -1,5 +0,0 @@
-# -*- encoding : utf-8 -*-
-# Bootstrap will_paginate and set default settings
-WillPaginate::ViewHelpers.pagination_options[:previous_label] = 'Föregående'
-WillPaginate::ViewHelpers.pagination_options[:next_label] = 'Nästa'
-WillPaginate::ViewHelpers.pagination_options[:renderer] = 'KPWillPaginate::LinkRenderer'
--- lib/kp_will_paginate.rb
+++ lib/kp_will_paginate.rb
@@ -4,39 +4,36 @@ module KPWillPaginate
   # LinkRenderer for WillPaginate that implements the markup guidelines for malmo.se.
   #
   # Renders the pagination links using a list.
-  class LinkRenderer < WillPaginate::LinkRenderer
-
-    attr_accessor :gap_marker
-
-    # Redefines the gap marker to be a list entry
-    def initialize
-      @gap_marker = '<li><span class="gap">&hellip;</span></li>'
-    end
-
-    # Renders the actual list of paging links.
-    #
-    # The method is essentially copied from WillPaginate::LinkRenderer with a <tt>ul</tt> tag added
-    def to_html
-      links = @options[:page_links] ? windowed_links : []
-      # previous/next buttons
-      links.unshift page_link_or_span(@collection.previous_page, 'disabled prev_page', @options[:previous_label])
-      links.push    page_link_or_span(@collection.next_page,     'disabled next_page', @options[:next_label])
-
-      html = links.join(@options[:separator])
-      @options[:container] ? @template.content_tag(:ul, html, html_attributes) : html
-    end
-
+  class LinkRenderer < WillPaginate::ActionView::LinkRenderer
 
     protected
 
-    # Adds a <tt>li</tt>-tag surrounding the pagination link
-    def page_link(page, text, attributes = {})
-      @template.content_tag(:li, @template.link_to(text, url_for(page), attributes))
+    # The container is a list
+    def html_container(html)
+      tag(:ul, html, container_attributes)
     end
 
-    # Adds a <tt>li</tt>-tag surrounding the page indicator
-    def page_span(page, text, attributes = {})
-      @template.content_tag(:li, @template.content_tag(:span, text, attributes))
+    # Redefines the gap marker to be a list entry
+    def gap
+      '<li><span class="gap">&hellip;</span></li>'
+    end
+
+    # Wrap page numbers in list entries
+    def page_number(page)
+      unless page == current_page
+        tag(:li, link(page, page, :rel => rel_value(page)))
+      else
+        tag(:li, tag(:span, page, :class => 'current'))
+      end
+    end
+
+    # Wrap previous/next links in list entries
+    def previous_or_next_page(page, text, classname)
+      if page
+        tag(:li, link(text, page, :class => classname))
+      else
+        tag(:li, tag(:span, text, :class => classname + ' disabled'))
+      end
     end
   end
 end
