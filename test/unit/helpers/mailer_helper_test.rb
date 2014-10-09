6ad47284945d0aba10ad6de489261c6a9745add4
Ola Olbonius <ola.olbonius@kentor.se>
2014-10-09 18:24:33 +0200

    Ändrat länk i mailutskick för att SiteVision skall kunna peka rätt.

--- test/unit/helpers/mailer_helper_test.rb
+++ test/unit/helpers/mailer_helper_test.rb
@@ -3,6 +3,6 @@ require 'test_helper'
 class MailerHelperTest < ActionView::TestCase
   test "mail url" do
     APP_CONFIG.replace(kp_external_link: "test")
-    assert_equal "test?goto=%2Flogin", mail_url("/login")
+    assert_equal "test/login", mail_url("/login")
   end
 end
