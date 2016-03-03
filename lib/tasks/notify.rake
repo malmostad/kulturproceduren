d058b6c058818bc6eec0d1e9a9148200a66b104c
Ted Parnefors <ted.parnefors@kentor.se>
2016-03-03 08:30:46 +0100

    Lagt till lite texter till loggning vid exekvering av rake_tasks.sh

--- lib/tasks/notify.rake
+++ lib/tasks/notify.rake
@@ -11,7 +11,6 @@ namespace :kp do
     SendAnswerForms.new(Date.today, APP_CONFIG[:evaluation_form][:activation_days]).run
   end
 
-
   desc "Reminds a companion to fill in the evaluation form"
   task(remind_answer_form: :environment) do
     RemindAnswerForm.new(Date.today, APP_CONFIG[:evaluation_form][:reminder_days]).run
@@ -19,7 +18,9 @@ namespace :kp do
 
   desc "Sends a notification for ticket release"
   task(notify_ticket_release: :environment) do
+    puts "NotifyTicketRelease.new.run starting"
     NotifyTicketRelease.new.run
+    puts "NotifyTicketRelease.new.run ended"
   end
 
 end
--- script/rake_tasks.sh
+++ script/rake_tasks.sh
@@ -23,11 +23,23 @@ cd $RAILS_DIR
 
 echo -n "Begin rake tasks KP, RAILS_ENV=$RAILS_ENV at "
 date
+
+echo "-- kp:notify_occasion_reminder"
 bundle exec rake RAILS_ENV=$RAILS_ENV kp:notify_occasion_reminder
+
+echo "-- kp:notify_ticket_release"
 bundle exec rake RAILS_ENV=$RAILS_ENV kp:notify_ticket_release
+
+echo "-- kp:remind_answer_form"
 bundle exec rake RAILS_ENV=$RAILS_ENV kp:remind_answer_form
+
+echo "-- kp:send_answer_forms"
 bundle exec rake RAILS_ENV=$RAILS_ENV kp:send_answer_forms
+
+echo "-- kp:update_tickets"
 bundle exec rake RAILS_ENV=$RAILS_ENV kp:update_tickets
+
+echo "-- kp:send_bus_bookings"
 bundle exec rake RAILS_ENV=$RAILS_ENV kp:send_bus_bookings
 
 echo -n "all tasks done at "
