c670617863e259e4e6d131fe3c726f945aebc64a
Ted Parnefors <ted.parnefors@kentor.se>
2016-03-04 17:51:24 +0100

    Justerat sökväg för bundle, så att cron-jobb skall funka bättre

--- script/rake_tasks.sh
+++ script/rake_tasks.sh
@@ -25,22 +25,22 @@ echo -n "Begin rake tasks KP, RAILS_ENV=$RAILS_ENV at "
 date
 
 echo "-- kp:notify_occasion_reminder"
-bundle exec rake RAILS_ENV=$RAILS_ENV kp:notify_occasion_reminder
+/home/kp-production/.rbenv/shims/bundle exec rake RAILS_ENV=$RAILS_ENV kp:notify_occasion_reminder
 
 echo "-- kp:notify_ticket_release"
-bundle exec rake RAILS_ENV=$RAILS_ENV kp:notify_ticket_release
+/home/kp-production/.rbenv/shims/bundle exec rake RAILS_ENV=$RAILS_ENV kp:notify_ticket_release
 
 echo "-- kp:remind_answer_form"
-bundle exec rake RAILS_ENV=$RAILS_ENV kp:remind_answer_form
+/home/kp-production/.rbenv/shims/bundle exec rake RAILS_ENV=$RAILS_ENV kp:remind_answer_form
 
 echo "-- kp:send_answer_forms"
-bundle exec rake RAILS_ENV=$RAILS_ENV kp:send_answer_forms
+/home/kp-production/.rbenv/shims/bundle exec rake RAILS_ENV=$RAILS_ENV kp:send_answer_forms
 
 echo "-- kp:update_tickets"
-bundle exec rake RAILS_ENV=$RAILS_ENV kp:update_tickets
+/home/kp-production/.rbenv/shims/bundle exec rake RAILS_ENV=$RAILS_ENV kp:update_tickets
 
 echo "-- kp:send_bus_bookings"
-bundle exec rake RAILS_ENV=$RAILS_ENV kp:send_bus_bookings
+/home/kp-production/.rbenv/shims/bundle exec rake RAILS_ENV=$RAILS_ENV kp:send_bus_bookings
 
 echo -n "all tasks done at "
 date
