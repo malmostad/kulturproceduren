52e1180439392f549b7f8c19b3d098da0cc74076
Ted Parnefors <ted.parnefors@kentor.se>
2016-06-15 13:12:09 +0200

    #1745 Lagt till modell för ålderskategorier

--- app/models/age_category.rb
+++ app/models/age_category.rb
@@ -1,2 +1,3 @@
 class AgeCategory < ActiveRecord::Base
+  attr_accessible :name, :from_age, :to_age
 end
--- db/migrate/20160615102814_create_age_categories.rb
+++ db/migrate/20160615102814_create_age_categories.rb
@@ -7,5 +7,11 @@ class CreateAgeCategories < ActiveRecord::Migration
 
       t.timestamps
     end
+
+    AgeCategory.create!(name: 'Förskola', from_age: 1, to_age: 5)
+    AgeCategory.create!(name: 'F-klass – skolår 3', from_age: 6, to_age: 9)
+    AgeCategory.create!(name: 'Skolår 4-6', from_age: 10, to_age: 12)
+    AgeCategory.create!(name: 'Skolår 7-9', from_age: 13, to_age: 15)
+    AgeCategory.create!(name: 'Gymnasiet', from_age: 16, to_age: 19)
   end
 end
